9d10aca270527b1edcabc226d3c0dd42dc61d2f5
sanasharathchandra <93203011+sanasharathchandra@users.noreply.github.com>
2024-05-26 20:15:24 +0000

    added solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+   const[name,setName]=useState("");
+
+   const handleNameChanger=(event)=>{
+    setName(event.target.value);
+   }
   return (
+
     <div>
         {/* Do not remove the main div */}
+
+   <p> Enter your name:</p>
+   <input 
+     type="text"
+     value={name}
+     onChange={handleNameChanger}
+     />
+   <p>{name}? `Hello ${name}!`:" "</p>
     </div>
   )
 }
